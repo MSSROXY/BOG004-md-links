e7a04bcc159f1ed26103c03b32b41f4bc2360e27
mssroxy <roxysolanopino@gmail.com>
2022-04-26 13:05:21 -0400

    funcionalidad de averiguar la extension del archivo que ingresa el usuario en consola

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 var clc = require('cli-color');
+var path = require('path');
 const [, ,route] = process.argv;
 
-console.log(clc.red.bgWhite('esta es la ruta a analizar: ', route))
+console.log(clc.red.bgWhite('esta es la ruta a analizar:', route));
+
 
 fs.promises.readFile(route)
   .then((result) => {
-    console.log(result.toString())
+    console.log(clc.red.bgWhite('esta es la extensión del archivo:', path.extname(route)))
+    console.log(result.toString());
   })
   .catch((error) => {
     console.log('este es el error: ', error)
