98b12d55c24bfef1901de536c345699203298ec8
mssroxy <roxysolanopino@gmail.com>
2022-04-26 12:04:30 -0400

    funcionalidad de leer el archivo ingresando en consola la ruta

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+var clc = require('cli-color');
+const [, ,route] = process.argv;
+
+console.log(clc.red.bgWhite('esta es la ruta a analizar: ', route))
+
+fs.promises.readFile(route)
+  .then((result) => {
+    console.log(result.toString())
+  })
+  .catch((error) => {
+    console.log('este es el error: ', error)
+  });
+
+
+
+// module.exports = () => {
+//     // ...
+// };
