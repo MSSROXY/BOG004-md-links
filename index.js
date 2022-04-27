a18b9a97e0183c2d8d0e9742a9764f840a1344d5
mssroxy <roxysolanopino@gmail.com>
2022-04-27 11:42:45 -0400

    funcionalidad de enlistar lo que contiene un directorio

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const fs = require('fs');
 var clc = require('cli-color');
 var path = require('path');
-const [, ,route] = process.argv;
+const [, ,route, directory] = process.argv;
 
-console.log(clc.red.bgWhite('esta es la ruta a analizar:', route));
-
+console.log(clc.red.bgWhite('Esta es la ruta a analizar:', route));
+console.log(clc.blue.bgWhite('Y este es el directorio a enlistar:', directory))
+var files = fs.readdirSync(directory);
 
 fs.promises.readFile(route)
   .then((result) => {
     console.log(clc.red.bgWhite('esta es la extensión del archivo:', path.extname(route)))
     console.log(result.toString());
+    console.log(clc.blue.bgWhite('esto es el contenido del directorio:', files))
   })
   .catch((error) => {
     console.log('este es el error: ', error)
