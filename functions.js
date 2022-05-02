059a16e23763031540f3643f12600385f40d20f4
mssroxy <roxysolanopino@gmail.com>
2022-05-02 12:21:43 -0400

    promesa de leer archivos md

--- functions.js
+++ functions.js
@@ -5,39 +5,55 @@ var clc = require('cli-color');
 const [, ,route] = process.argv;
 
 // función para convertir ruta relativa en absoluta
-const routeAbsolute = (route) => {
-    if(path.isAbsolute(route)){
-        return route;
+const routeAbsolute = (userRoute) => {
+    if(path.isAbsolute(userRoute)){
+        return userRoute;
     }
     else {
-        const absPath = path.resolve(route).normalize();
+        const absPath = path.resolve(userRoute).normalize();
         return absPath;
     }
 };
 
-// guardo el resultado de la función 
+// guardo el resultado de la función en una variable
 let myRoute = routeAbsolute(route);
 
 // función recursiva para enlistar los archivos.md de un directorio en un array
 // si se ingresa un archivo.md solo muestra el archivo
 
-const listMDfiles = (myRoute) => {
-    let myMDfiles = [];
-    if(fs.statSync(myRoute).isFile() && path.extname(myRoute) === '.md'){
-        myMDfiles.push(myRoute);
+const listMDfiles = (userRoute) => {
+    let MDfiles = [];
+    if(fs.statSync(userRoute).isFile() && path.extname(userRoute) === '.md'){
+        MDfiles.push(userRoute);
     }
     else{
-        if(fs.statSync(myRoute).isDirectory()){
-            const directory = myRoute;
+        if(fs.statSync(userRoute).isDirectory()){
+            const directory = userRoute;
             let contentDirectory = fs.readdirSync(directory);
             contentDirectory.forEach(elem => {
-                listMDfiles(myRoute + '/' + elem).forEach(elem => {
-                    myMDfiles.push(elem);
+                listMDfiles(userRoute + '/' + elem).forEach(elem => {
+                    MDfiles.push(elem);
                 })
             })
         }
     }
-    return myMDfiles;
+    return MDfiles;
 };
 
-console.log(clc.red.bgWhite('este es el resultado de la f recursiva: '), listMDfiles(myRoute));
+const myMDfiles = listMDfiles(myRoute);
+console.log(clc.red.bgWhite('este es el resultado de la f recursiva: '), myMDfiles);
+
+// empezamos con la promesa para leer los archivos que estan en un array
+const readMDfiles = (MDarray) => {
+    MDarray.forEach(md => {
+        fs.promises.readFile(md, 'utf-8')
+        .then((result)=> {
+            console.log(clc.red.bgWhite('este es el contenido de cada archivo.md'), result)
+        })
+        .catch((error)=>{
+            console.log('este es el error', error);
+        })
+    })
+}
+
+readMDfiles(myMDfiles)
